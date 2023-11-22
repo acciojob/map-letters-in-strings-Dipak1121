5571b0bac835f9d0d7ec3d59b142d983a9e15232
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-22 13:25:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function mapChars(str){
+	let obj = {};
+	for( let i = 0; i < str.length;  i++ ){
+		if(obj[str[i]] != undefined){
+			obj[str[i]].push(i);
+		}
+		else{
+			obj[str[i]] = [i];
+		}
+	}
+	return obj;
+}
+
+const str = prompt("Enter the string");
+alert(mapChars(str));
